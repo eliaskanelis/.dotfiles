bfac75ea420cb064d77aeac909471dc324940f76
tedi <hkanelhs@yahoo.gr>
2020-03-01 19:42:35 +0200

    Added flameshot

--- core/scripts/system/printscreen.sh
+++ core/scripts/system/printscreen.sh
@@ -2,16 +2,13 @@
 
 # Print the screen
 
-# Check if tool is available
-if ! [ -x "$(command -v scrot)" ]; then
-    notify-send -u critical "Printscreen failed" "scrot is not installed"
-    exit 1
-fi
+PRINTSCEEN=flameshot
 
-if ! [ -x "$(command -v gimp)" ]; then
-    notify-send -u critical "Printscreen failed" "gimp is not installed"
+# Check if tool is available
+if ! [ -x "$(command -v ${PRINTSCEEN})" ]; then
+    notify-send -u critical "Printscreen failed" "${PRINTSCEEN} is not installed"
     exit 1
 fi
 
 # Run app
-scrot -e 'mv $f /tmp/ && gimp /tmp/$f' --quality 100
+${PRINTSCEEN} gui
