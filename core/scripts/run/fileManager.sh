e3bae7ac24ed458746764221a655e8dbf5d3e91e
tedi <hkanelhs@yahoo.gr>
2021-10-17 12:29:12 +0300

    Addition of ~/dow and corrections on filemanager script

--- core/scripts/run/fileManager.sh
+++ core/scripts/run/fileManager.sh
@@ -6,6 +6,9 @@
 #fileManager=nemo
 fileManager=thunar
 
+# Optional arguments
+input_path="$1"
+
 # Check if tool is available
 if ! [ -x "$(command -v i3-sensible-terminal)" ]; then
     notify-send -u critical -i "notification-message-IM" "i3-sensible-terminal is not installed"
@@ -17,6 +20,12 @@ if ! [ -x "$(command -v ${fileManager})" ]; then
     exit 1
 fi
 
+path="${HOME}"/Desktop/
+if [[ "${input_path}" != "" ]]
+then
+    # Relative to absolute
+    path="$(cd "$(dirname "${input_path}")"; pwd)/$(basename "${input_path}")"
+fi
+
 # Run app
-#i3-sensible-terminal -e ${fileManager}
-${fileManager} ${HOME}/Desktop/
+${fileManager} "${path}"
